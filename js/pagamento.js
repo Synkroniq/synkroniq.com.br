6353bcd9e9f50002e3571ed5d8651af9fba6413a
SYNKRONIQ <eduachou@gmail.com>
2025-09-16 22:26:10 -0300

    Update pagamento.js

--- js/pagamento.js
+++ js/pagamento.js
@@ -1,5 +1,5 @@
 function copiarPix() {
-  const chave = document.getElementById("chavePix").textContent;
+  const chave = "88a7f686-2995-4c51-b787-37c06755b569";
   navigator.clipboard.writeText(chave).then(() => {
     alert("Chave Pix copiada com sucesso!");
   });
