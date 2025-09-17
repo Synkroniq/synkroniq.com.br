95a42f63f5d0cc88e355662c75c748d32a674bf1
SYNKRONIQ <eduachou@gmail.com>
2025-09-17 01:28:37 -0300

    Update contato.js

--- js/contato.js
+++ js/contato.js
@@ -1,5 +1,5 @@
 function abrirWhatsApp() {
-  const numero = "5544997648490"; // Exemplo: 5544999999999
+  const numero = "+5544997648490"; // ← Substitua pelo número real com DDI + DDD
   const mensagem = "Solicitação de Suporte";
   const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
   window.open(url, '_blank');
