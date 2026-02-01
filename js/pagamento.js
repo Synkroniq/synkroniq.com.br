7d283ff3d1e247abb6431bcc828b129089bbcd9c
SYNKRONIQ <eduachou@gmail.com>
2026-02-01 01:41:42 -0300

    Delete js/pagamento.js

--- js/pagamento.js
+++
@@ -1,6 +0,0 @@
-function copiarPix() {
-  const chave = "88a7f686-2995-4c51-b787-37c06755b569";
-  navigator.clipboard.writeText(chave).then(() => {
-    alert("Chave Pix copiada com sucesso!");
-  });
-}
